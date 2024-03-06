60783e998b8d5b9e41bd408a71bb843450adf7cb
Daniel Rodriguez <dan74109@gmail.com>
2024-03-06 17:32:58 -0500

    Creando directorios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarComponent } from './customers/car/car.component';
+import { ProductsComponent } from './customers/products/products.component';
+import { MenuComponent } from './admin/menu/menu.component';
+import { ProductComponent } from './admin/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    ProductsComponent,
+    MenuComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
