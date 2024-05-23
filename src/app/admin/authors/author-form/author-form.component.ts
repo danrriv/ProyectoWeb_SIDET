3bbd7022885edd98297a5a68a1d3a839bf08ef9e
Daniel Rodriguez <dan74109@gmail.com>
2024-05-22 23:31:43 -0500

    Finalizando con los estilos de Angular Material en autores

--- src/app/admin/authors/author-form/author-form.component.ts
+++ src/app/admin/authors/author-form/author-form.component.ts
@@ -1,33 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Author } from 'src/app/clases/author/author';
 import { AuthorsService } from 'src/app/services/api-authors/authors.service';
+import Swal from 'sweetalert2';
 import swal from 'sweetalert2';
 @Component({
   selector: 'app-author-form',
   templateUrl: './author-form.component.html',
   styleUrls: ['./author-form.component.css']
 })
-export class AuthorFormComponent {
+export class AuthorFormComponent implements OnInit {
 
-  public author:Author = new Author();
-  errorStatus: boolean = false;
-  errorMsj: any = "";
+  authorForm: FormGroup;
+  isNewAuthor: boolean = true;
+  authorId: number;
 
   constructor(
     private authorService: AuthorsService,
     private activatedRoute: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ){}
 
-  loadAuthors(): void{
+  ngOnInit(): void {
+      this.initForm();
+      this.loadAuthor();
+  }
+
+  initForm(): void {
+    this.authorForm = this.formBuilder.group({
+      author_name: ['', Validators.required]
+    });
+  }
+
+  loadAuthor(): void{
     this.activatedRoute.params.subscribe((params) =>{
       const id = +params['id'];
-  
       if(id){
+        this.isNewAuthor = false;
+        this.authorId = id;
         this.authorService.findAuthorId(id).subscribe(
           (data) => {
-            this.author = data;
+            this.authorForm.patchValue({
+              author_name: data.author_name
+            });
           },
           (error) => {
             console.error(error);
@@ -37,32 +54,35 @@ export class AuthorFormComponent {
     });
   }
 
-  
-  saveAuthor(){
-    this.authorService.saveAuthor(this.author).subscribe(
-      (author) =>{
-        this.router.navigate(['/mundo-literario/admin/mantenimiento-autor']);
-        swal.fire(
-          'Éxito!',
-          'Autor registrado correctamente',
-          'success'
-        )
-      }, error => console.log(error));
+  public saveAuthor(): void {
+    if (this.authorForm.valid) {
+      const authorData = this.authorForm.value;
+      const author: Author = {
+        author_id: this.authorId,
+        author_name: authorData.authot_name
+      };
+
+      if (this.isNewAuthor) {
+        this.authorService.saveAuthor(author).subscribe(
+          () => {
+            this.router.navigate(['/mundo-literario/admin/mantenimiento-categorias']);
+            Swal.fire('Éxito!', 'Autor registrado correctamente', 'success');
+          },
+          (error) => {
+            console.error(error);
+          }
+        );
+      } else {
+        this.authorService.updateAuthor(author).subscribe(
+          () => {
+            this.router.navigate(['/mundo-literario/admin/mantenimiento-categorias']);
+            Swal.fire('Éxito!', 'Autor actualizado correctamente', 'success');
+          },
+          (error) => {
+            console.error(error);
+          }
+        );
+      }
+    } 
   }
-  
-
-  updateAuthor(){
-    this.authorService.updateAuthor(this.author).subscribe(
-      (author) =>{
-        this.router.navigate(['/mundo-literario/admin/mantenimiento-autor']);
-        swal.fire(
-          'Éxito!',
-          'Autor actualizado correctamente',
-          'success'
-        )
-      }, error => console.log(error));
-  }
-
-
-  
 }
