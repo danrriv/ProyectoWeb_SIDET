13fb8476c9cd89d6085ee0e8c5eb7b956c7b3f93
ClaudiaYF-coder <107971435+ClaudiaYF-coder@users.noreply.github.com>
2024-06-03 15:43:31 -0500

    cambios en menu

--- src/app/customers/templates/header/header.component.ts
+++ src/app/customers/templates/header/header.component.ts
@@ -5,41 +5,46 @@ import { SalesService } from 'src/app/services/api-sales/sales.service';
 import Swal from 'sweetalert2';
 import { ApiCategoriesService } from 'src/app/services/api-categories/api-categories.service';
 import { MenuData } from 'src/app/clases/menuData/menu-data';
-
+import { Customer } from 'src/app/clases/customer/customer';
+ 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+ 
   menuData: any[] = [];
   categories: any = {};
+ 
   customerName: string;
+ 
   showMenu: boolean = false;
   showSubMenu: boolean = false;
+ 
+  customer:Customer = new Customer();
   viewCart: boolean = false;
   islogged: boolean = false;
   token: string | null = null;
   myCart$ = this.cartService.myCart$;
-
-
+ 
+ 
   constructor(
     private customerService: ApiCustomersService,
     private cartService: CartProductsService,
     private categoryService: ApiCategoriesService) { }
-
+ 
   ngOnInit(): void {
     this.token = localStorage.getItem('tokenCustomer');
     if (this.token != null) {
       this.islogged = true;
-      this.customerName = localStorage.getItem('name') || 'error';
+      this.setCustomerData();
     }
     this.categoryService.menu().subscribe((data: MenuData[]) => {
       this.menuData = data;
-
+ 
       let groupedData: any = {};
-
+ 
       for (let data of this.menuData) {
         if (!groupedData[data.category_name]) {
           groupedData[data.category_name] = {
@@ -47,29 +52,46 @@ export class HeaderComponent implements OnInit {
             genre: {}
           };
         }
-
+ 
         if (data.genre_name && !groupedData[data.category_name].genre[data.genre_name]) {
           groupedData[data.category_name].genre[data.genre_name] = {
             name: data.genre_name,
             subgenre: []
           };
         }
-
+ 
         if (data.subgenre_name) {
           groupedData[data.category_name].genre[data.genre_name].subgenre.push(data.subgenre_name);
         }
       }
-
+ 
+      console.log(groupedData)
       // Convierte el objeto en un array
       this.menuData = Object.values(groupedData).map((category: any) => {
         return {
           ...category,
           genre: Object.values(category.genre)
         };
-      });
+      });+
+ 
+      console.log(this.menuData);
     });
   }
-
+ 
+  setCustomerData(){
+    if(this.token){
+      this.customerService.obtainProfile(this.token).subscribe(
+        (data) => {
+         this.customer = data;
+         localStorage.setItem('idCustomer',this.customer.customer_id);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    }
+  }
+ 
 logout() {
   Swal.fire({
     title: '¿Estás seguro?',
@@ -85,16 +107,16 @@ logout() {
     }
   });
 }
-
+ 
 //Manejo del menú dinámico
 getKeys(obj: any): string[] {
   return Object.keys(obj);
 }
-
+ 
 toggleMenu() {
   this.showMenu = !this.showMenu;
 }
-
+ 
 toggleSubMenu() {
   if (this.showMenu) {
     this.showSubMenu = !this.showSubMenu;
@@ -104,5 +126,5 @@ toggleSubMenu() {
 onToggleCart() {
   this.viewCart = !this.viewCart;
 };
-
+ 
 }
