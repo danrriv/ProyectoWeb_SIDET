a4c6583d7d2df9855277db9990db1488f02cc82f
Daniel Rodriguez <dan74109@gmail.com>
2024-06-26 19:26:49 -0500

    Agregando reporte de ventas

--- src/app/admin/reports/sale-details-report/sale-details-report.component.ts
+++ src/app/admin/reports/sale-details-report/sale-details-report.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
+import * as XLSX from 'xlsx';
+import { SaleL } from 'src/app/clases/sales/sale/sale-l';
+import { SaleDetailsL } from 'src/app/clases/sales/saleDetails/sale-details-l';
+import { SalesService } from 'src/app/services/api-sales/sales.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-sale-details-report',
   templateUrl: './sale-details-report.component.html',
   styleUrls: ['./sale-details-report.component.css']
 })
-export class SaleDetailsReportComponent {
+export class SaleDetailsReportComponent implements OnInit {
+
+  saleDetails: SaleDetailsL[];
+  dataSource: MatTableDataSource<SaleDetailsL>;
+  sale: SaleL;
+  id: number;
+
+
+
+  constructor(private saleService: SalesService,
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+
+  ngOnInit(): void {
+    this.getDetails();
+    this.getSale();
+
+  }
+
+  getDetails(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      const id = +params['id'];
+      if (id) {
+        this.saleService.findDetails(id).subscribe((data: SaleDetailsL[]) => {
+          this.saleDetails = data;
+          this.dataSource = new MatTableDataSource<SaleDetailsL>(this.saleDetails)
+        }, (error) => {
+          console.log(error)
+        })
+      }
+    })
+  }
+
+  getSale(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      const id = +params['id'];
+      if (id) {
+        this.saleService.findSale(id).subscribe((data) => {
+          return this.sale = data;
+        })
+      }
+    })
+  }
+
+  exportExcel(): void {
+    let ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.saleDetails);
+
+    let wb: XLSX.WorkBook = XLSX.utils.book_new();
+
+    XLSX.utils.book_append_sheet(wb, ws, 'Detalles');
+
+    XLSX.writeFile(wb, 'reporte_dventas.xlsx');
+  }
+
+
+
+
+
 
 }
